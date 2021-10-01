1db24a3dd425f87e067ccf30ab0dbc0a3edf11db
taskinhasan <91372178+taskinhasan@users.noreply.github.com>
2021-10-01 14:35:58 +0300

    Add files via upload

---
+++ reCapDemo1/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x = 36;
+    int y = 20;
+    int z = 35;
+    int enBuyuk = x;
+
+
+    if(enBuyuk<y){
+        enBuyuk = y;
+    }
+    if(enBuyuk<z){
+        enBuyuk = z;
+    }
+
+    System.out.println("En büyük sayı ="+ enBuyuk);
+
+
+    //  if(x>y && x>z) {
+    //    System.out.println("En büyük sayı x'tir.");
+    // }
+    //  else if (y>x && y>z) {
+    //    System.out.println("En büyük sayı y'dir.");
+
+    // }
+    //  else {
+    //    System.out.println("En büyük sayı z'dir.");
+
+     //   }
+
+
+
+
+
+    }
+
+}
